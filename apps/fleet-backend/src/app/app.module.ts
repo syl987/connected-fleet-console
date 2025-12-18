a0d0b3f777434ed12380704e1ad43422397119e2
Igor Milly <igor.milly@protonmail.com>
2025-12-18 17:51:42 +0100

    feat: add typeorm in-memory database

--- apps/fleet-backend/src/app/app.module.ts
+++ apps/fleet-backend/src/app/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      synchronize: true,
+      entities: [],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
