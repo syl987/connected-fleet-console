39e07b0cc60c3456d1b349787a61891b767f4361
Igor Milly <igor.milly@protonmail.com>
2025-12-19 00:46:48 +0100

    refactor(webapp): misc renaming

--- apps/fleet-webapp/src/app/store/effects/undo.effects.ts
+++ apps/fleet-webapp/src/app/store/effects/undo.effects.ts
@@ -6,17 +6,17 @@ import { map } from 'rxjs';
 @Injectable()
 export class UndoEffects {
   protected readonly actions = inject<Actions<EntityAction>>(Actions);
-  protected readonly f = inject(EntityActionFactory);
+  protected readonly factory = inject(EntityActionFactory);
 
   readonly undoOne$ = createEffect(() =>
     this.actions.pipe(
       ofEntityOp(EntityOp.SAVE_ADD_ONE_ERROR, EntityOp.SAVE_UPDATE_ONE_ERROR, EntityOp.SAVE_UPSERT_ONE_ERROR, EntityOp.SAVE_DELETE_ONE_ERROR),
-      map(action => this.f.createFromAction(action, { entityOp: EntityOp.UNDO_ONE, data: action.payload.data.originalAction.payload.data })),
+      map(action => this.factory.createFromAction(action, { entityOp: EntityOp.UNDO_ONE, data: action.payload.data.originalAction.payload.data })),
     ));
 
   readonly undoMany$ = createEffect(() =>
     this.actions.pipe(
       ofEntityOp(EntityOp.SAVE_ADD_MANY_ERROR, EntityOp.SAVE_UPDATE_MANY_ERROR, EntityOp.SAVE_UPSERT_MANY_ERROR, EntityOp.SAVE_DELETE_MANY_ERROR),
-      map(action => this.f.createFromAction(action, { entityOp: EntityOp.UNDO_MANY, data: action.payload.data.originalAction.payload.data })), // not tested
+      map(action => this.factory.createFromAction(action, { entityOp: EntityOp.UNDO_MANY, data: action.payload.data.originalAction.payload.data })), // not tested
     ));
 }
