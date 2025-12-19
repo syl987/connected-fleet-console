12d574544374f1b6e19205de344c4cb23884f678
Igor Milly <igor.milly@protonmail.com>
2025-12-19 12:08:08 +0100

    feat: misc progress

--- apps/fleet-webapp/src/app/components/main/logs-search-page/logs-search-page.component.ts
+++ apps/fleet-webapp/src/app/components/main/logs-search-page/logs-search-page.component.ts
@@ -1,10 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { logsFeature } from '../../../store/logs/logs.reducer';
 import { TitleBarComponent } from '../../core/title-bar/title-bar.component';
 
 @Component({
   selector: 'app-logs-search-page',
-  imports: [TitleBarComponent],
+  imports: [ReactiveFormsModule, TitleBarComponent],
   templateUrl: './logs-search-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LogsSearchPageComponent {}
+export class LogsSearchPageComponent {
+  protected readonly store = inject(Store);
+
+  readonly form = new FormGroup({
+    query: new FormControl<string | null>(''),
+    severity: new FormControl<string | null>(''),
+    from: new FormControl<string | null>(''),
+    to: new FormControl<string | null>(''),
+  });
+
+  readonly logs = this.store.selectSignal(logsFeature.selectAll);
+  readonly searching = this.store.selectSignal(logsFeature.selectSearching);
+}
