f0d234aec26c2df4f74caede4185b651bf90584d
Igor Milly <igor.milly@protonmail.com>
2025-12-19 16:44:23 +0100

    sdfg

--- apps/fleet-webapp/src/app/components/vehicles/vehicle-detail-page/vehicle-detail-page.component.ts
+++ apps/fleet-webapp/src/app/components/vehicles/vehicle-detail-page/vehicle-detail-page.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
@@ -25,18 +25,10 @@ import { VehicleCardComponent } from '../vehicle-card/vehicle-card.component';
   templateUrl: './vehicle-detail-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VehicleDetailPageComponent implements OnInit {
+export class VehicleDetailPageComponent {
   protected readonly service = inject(VehicleService);
   protected readonly route = inject(ActivatedRoute);
 
   readonly vehicle = toSignal(this.service.entityByRouteId$, { requireSync: true });
   readonly loading = toSignal(this.service.loading$, { requireSync: true });
-
-  ngOnInit() {
-    this.service.upsertOneInCache({ id: 0, brand: 'Toyota', model: 'Camry', year: 2020, mileage: 15000, vin: '1HGCM82633A123456' } as any);
-    this.service.upsertOneInCache({ id: 1, brand: 'Honda', model: 'Civic', year: 2021, mileage: 12000, vin: '2HGCM82633A123457' } as any);
-    this.service.upsertOneInCache({ id: 2, brand: 'Ford', model: 'F-150', year: 2019, mileage: 25000, vin: '3HGCM82633A123458' } as any);
-    this.service.upsertOneInCache({ id: 3, brand: 'BMW', model: 'X5', year: 2022, mileage: 8000, vin: '4HGCM82633A123459' } as any);
-    this.service.upsertOneInCache({ id: 4, brand: 'Tesla', model: 'Model S', year: 2021, mileage: 5000, vin: '5HGCM82633A123460' } as any);
-  }
 }
