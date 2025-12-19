a750acaceed02ee707bd48411162e9a13652389b
Igor Milly <igor.milly@protonmail.com>
2025-12-19 14:18:24 +0100

    dfsgsdh

--- apps/fleet-backend/src/app/modules/vehicles/vehicles.module.ts
+++ apps/fleet-backend/src/app/modules/vehicles/vehicles.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { VehiclesController } from './controllers/vehicles.controller';
 import { Vehicle } from './entities/vehicle.entity';
-import { VehicleImagesService } from './services/vehicle-images.service';
 import { VehiclesService } from './services/vehicles.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Vehicle])],
-  providers: [VehiclesService, VehicleImagesService],
+  providers: [VehiclesService],
   controllers: [VehiclesController],
-  exports: [VehiclesService, VehicleImagesService],
+  exports: [VehiclesService],
 })
 export class VehiclesModule {}
