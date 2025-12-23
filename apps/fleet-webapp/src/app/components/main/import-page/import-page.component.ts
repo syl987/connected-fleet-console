6a2fbd10d312c5c6892184472cbfcedbc014e1bc
Igor Milly <igor.milly@protonmail.com>
2025-12-23 14:44:53 +0100

    sdfgsdfg

--- apps/fleet-webapp/src/app/components/main/import-page/import-page.component.ts
+++ apps/fleet-webapp/src/app/components/main/import-page/import-page.component.ts
@@ -1,13 +1,33 @@
-import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, inject, viewChild } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { VehicleLog } from '../../../models/vehicle-log.models';
 import { ImportService } from '../../../services/import.service';
 import { TitleBarComponent } from '../../core/title-bar/title-bar.component';
 
+const tableColumns = [
+  'timestamp',
+  'severity',
+  'code',
+  'message',
+];
+
 @Component({
   selector: 'app-import-page',
-  imports: [MatButtonModule, TitleBarComponent, JsonPipe],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    TitleBarComponent,
+  ],
   templateUrl: './import-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -17,6 +37,18 @@ export class ImportPageComponent {
   readonly vehicleLogs = toSignal(this.importService.vehicleLogs$, { requireSync: true });
   readonly loading = toSignal(this.importService.loading$, { requireSync: true });
   readonly loaded = toSignal(this.importService.loaded$, { requireSync: true });
+
+  readonly paginator = viewChild(MatPaginator);
+
+  readonly tableColumns = tableColumns;
+  readonly dataSource = new MatTableDataSource<VehicleLog>([]);
+
+  constructor() {
+    effect(() => {
+      this.dataSource.data = this.vehicleLogs();
+      this.dataSource.paginator = this.paginator();
+    });
+  }
 
   import(file: File): void {
     this.importService.import(file);
